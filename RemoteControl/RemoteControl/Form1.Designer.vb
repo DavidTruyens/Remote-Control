0c8bbd59d1dadbd1b09f3c5ed604d54c5bd3b277
DavidTruyens <david.truyens@gmail.com>
2015-08-15 17:17:56 +0200

    Start

---
+++ RemoteControl/RemoteControl/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPortList = New System.Windows.Forms.ComboBox()
+        Me.ButtonDown = New System.Windows.Forms.Button()
+        Me.ButtonUp = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.SuspendLayout()
+        '
+        'SerialPortList
+        '
+        Me.SerialPortList.FormattingEnabled = True
+        Me.SerialPortList.Location = New System.Drawing.Point(3, 41)
+        Me.SerialPortList.Name = "SerialPortList"
+        Me.SerialPortList.Size = New System.Drawing.Size(178, 21)
+        Me.SerialPortList.TabIndex = 5
+        '
+        'ButtonDown
+        '
+        Me.ButtonDown.Location = New System.Drawing.Point(95, 12)
+        Me.ButtonDown.Name = "ButtonDown"
+        Me.ButtonDown.Size = New System.Drawing.Size(86, 23)
+        Me.ButtonDown.TabIndex = 4
+        Me.ButtonDown.Text = "Volume Down"
+        Me.ButtonDown.UseVisualStyleBackColor = True
+        '
+        'ButtonUp
+        '
+        Me.ButtonUp.Location = New System.Drawing.Point(3, 12)
+        Me.ButtonUp.Name = "ButtonUp"
+        Me.ButtonUp.Size = New System.Drawing.Size(86, 23)
+        Me.ButtonUp.TabIndex = 3
+        Me.ButtonUp.Text = "Volume Up"
+        Me.ButtonUp.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(186, 76)
+        Me.Controls.Add(Me.SerialPortList)
+        Me.Controls.Add(Me.ButtonDown)
+        Me.Controls.Add(Me.ButtonUp)
+        Me.Name = "Form1"
+        Me.Text = "Remote Control"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SerialPortList As ComboBox
+    Friend WithEvents ButtonDown As Button
+    Friend WithEvents ButtonUp As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+End Class
---
+++ RemoteControl/RemoteControl/Form1.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.IO.Ports
+Imports System.Management
+
+
+Public Class Form1
+
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Long) As Integer
+    Private Const APPCOMMAND_VOLUME_MUTE As Integer = &H80000
+    Private Const APPCOMMAND_VOLUME_DOWN As Integer = &H90000
+    Private Const APPCOMMAND_VOLUME_UP As Integer = &HA0000
+    Private Const WM_APPCOMMAND As Integer = &H319
+
+    Delegate Sub StringInput(ByVal StringOut As String)
+
+    <DllImport("user32.dll")>
+    Public Shared Function SendMessageW(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Try
+            Dim searcher As New ManagementObjectSearcher("root\cimv2", "SELECT * FROM Win32_SerialPort")
+
+            For Each SerialPort As ManagementObject In searcher.Get()
+                SerialPortList.Items.Add(SerialPort("name"))
+            Next
+
+        Catch err As ManagementException
+            MessageBox.Show("An error occured while querying for WMI data: " & err.Message)
+
+        End Try
+
+        Dim arduino As Boolean = False
+
+        For Each item As Object In SerialPortList.Items
+
+            If item.ToString.Contains("Arduino") Then
+                SerialPortList.Text = item.ToString
+                arduino = True
+                Exit For
+            ElseIf item.ToString.Contains("Blu") Then
+                SerialPortList.Text = item.ToString
+                arduino = True
+                Exit For
+            End If
+        Next
+
+        If Not arduino Then
+            MsgBox("No Arduino can be found..")
+            SerialPortList.Text = SerialPortList.Items.Item(0).ToString
+            Me.BackColor = Color.Orange
+
+        End If
+
+        If arduino Then
+            connect()
+        End If
+
+    End Sub
+
+    Private Sub connect()
+        Dim portname As String
+        portname = "COM" & GetPortNumber()
+        'MsgBox(portname)
+
+        Try
+            SerialPort1.Portname = portname
+            SerialPort1.BaudRate = 9600
+            SerialPort1.Open()
+            Me.BackColor = Color.Green
+
+        Catch ex As Exception
+            MsgBox("Arduino detected, but no connection could be made")
+            Me.BackColor = Color.Red
+        End Try
+
+    End Sub
+
+    Private Function GetPortNumber() As String
+        Dim com As String = "COM"
+        Dim x As Integer = InStr(SerialPortList.Text, com)
+        Dim string_after As String = SerialPortList.Text.Substring(x + com.Length - 1)
+
+        Dim closingbracket As String = ")"
+        Dim y As Integer = InStr(string_after, closingbracket)
+        Dim portnumber As String = string_after.Substring(0, y - 1)
+        Return portnumber
+    End Function
+
+    'Private Sub ButtonDown_Click(sender As Object, e As EventArgs) Handles ButtonDown.Click
+    '    SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_DOWN))
+    'End Sub
+
+    'Private Sub ButtonUp_Click(sender As Object, e As EventArgs) Handles ButtonUp.Click
+    '    SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_UP))
+    'End Sub
+
+    Private Sub SerialPort1_DataRecieved(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim StringOut As String
+        StringOut = SerialPort1.ReadLine
+
+        Invoke(New StringInput(AddressOf CMDCom), StringOut)
+    End Sub
+
+    Private Sub CMDCom(ByVal StringOut As String)
+        If StringOut.Contains("UP") Then
+            SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_UP))
+        ElseIf StringOut.Contains("DOWN") Then
+            SendMessageW(Me.Handle, WM_APPCOMMAND, Me.Handle, New IntPtr(APPCOMMAND_VOLUME_DOWN))
+        End If
+    End Sub
+End Class
